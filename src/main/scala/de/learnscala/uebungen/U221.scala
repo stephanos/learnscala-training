72342163a1ca8fe696cbdf24797f69b972a48340
Stephan <stephan.behnke@googlemail.com>
2012-07-31 19:58:07 +0200

    commit

--- src/main/scala/de/learnscala/uebungen/U221.scala
+++ src/main/scala/de/learnscala/uebungen/U221.scala
@@ -4,9 +4,44 @@ import de.learnscala.base.Uebung
 
 object U221 extends Uebung(enabled = true) {
 
-    /* TODO: Tupel & Option mit Pattern match */
-
-    def swap(t: (Int, Int)) =
+    /**
+     * Tauscht die Elemente des Tuples.
+     */
+    def swap(t: (Int, Int)): (Int, Int) =
         (t._2, t._1)
+
+
+    /**
+     * Summiert die Elemente eines Tuples.
+     * Das erste Element zeigt an, ob addiert (true) oder subtrahiert (false) werden soll.
+     */
+    def sum(t: (Boolean, Int, Int)) =
+        t match {
+            case (true, a1, a2) => a1 + a2
+            case (false, a1, a2) => a1 - a2
+        }
+
+
+    /**
+     * Antwortet ob das Auto gekauft werden soll - abhängig vom Argument.
+     */
+    def doYouBuyTheCar(b: Option[Boolean]) {
+        print(b match {
+            case Some(true) => "yes"
+            case Some(false) => "no"
+            case _ => "not sure"
+        })
+    }
+
+
+    /**
+     * Konvertiert einen String in einen Integer.
+     */
+    def convertToInt(s: String): Option[Int] =
+        try {
+            Some(s.toInt)
+        } catch {
+            case _: Throwable => None
+        }
 
 }
