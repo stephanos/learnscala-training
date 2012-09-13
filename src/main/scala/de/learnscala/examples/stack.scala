baef7b5cf8e8d2b7e44e4a2cf794e7e238ec58c9
stephan <stephan@101loops.com>
2012-09-13 11:38:54 +0200

    commit

--- src/main/scala/de/learnscala/examples/stack.scala
+++ src/main/scala/de/learnscala/examples/stack.scala
@@ -21,10 +21,15 @@ object Main extends App {
 
     val s1 = new Stack[Int]
     s1.push(1)
+    s1.push('a')
+    println(s1.top)
     s1.pop()
+    println(s1.top)
 
     val s2 = new Stack[String]
     s2.push("A")
     s2.push("B")
+    println(s2.top)
     s2.pop()
+    println(s2.top)
 }
