cca609f534faf78109cea405148b2ff85c34283a
stephan <stephan@101loops.com>
2012-10-14 13:15:01 +0200

    commit

--- src/test/scala/de/learnscala/test/generic/Test_F24.scala
+++ src/test/scala/de/learnscala/test/generic/Test_F24.scala
@@ -5,4 +5,33 @@ import scala.reflect.runtime.universe._
 
 abstract class Test_F24[T: TypeTag] extends BaseTest[T] {
 
+    //        "titlesWithAuthorMartin must have titles of books with author 'Martin'" >> {
+    //            checkType[List[String]](titlesWithAuthorMartin, "titlesWithAuthorMartin", enabled) {
+    //                res =>
+    //                    res must containAllOf(List("Programming In Scala", "Patterns of Enterprise Application Architecture"))
+    //            }
+    //        }
+    //
+    //        "isbnsWithAuthorS must have ISBNs of books where author's lastname starts with 'S'" >> {
+    //            checkType[List[String]](isbnsWithAuthorS, "isbnsWithAuthorS", enabled) {
+    //                res =>
+    //                    res must containAllOf(List("3868020640", "0321127420"))
+    //            }
+    //        }
+    //
+    //        "priceAndTitleOfScalaBooks must have list of title and prices where 'Scala' is in the title" >> {
+    //            checkType[List[String]](priceAndTitleOfScalaBooks, "priceAndTitleOfScalaBooks", enabled) {
+    //                res =>
+    //                    res must containAllOf(List(("Programming In Scala" -> 36.95f), ("Programming Scala" -> 27.95f),
+    //                        ("Durchstarten mit Scala" -> 24.90f), ("Programming Scala" -> 22.95f)))
+    //            }
+    //        }
+    //
+    //        "titlesOutOfStock " >> {
+    //            checkType[List[String]](titlesOutOfStock, "titlesOutOfStock", enabled) {
+    //                res =>
+    //                    res must containAllOf(List("Java Virtual Machine", "Patterns of Enterprise Application Architecture"))
+    //            }
+    //        }
+    //    }
 }
--- src/test/scala/de/learnscala/test/generic/Test_O22.scala
+++ src/test/scala/de/learnscala/test/generic/Test_O22.scala
@@ -5,5 +5,26 @@ import scala.reflect.runtime.universe._
 
 abstract class Test_O22[T: TypeTag] extends BaseTest[T] {
 
+    //        "sum" >> {
+    //            "must return '5' for Leaf(5)" >> {
+    //                s1 === 5
+    //            }
+    //            "must return '5' for Node('+', Leaf(5), Leaf(5))" >> {
+    //                s2 === 10
+    //            }
+    //            "must return '5' for Node('*', Leaf(5), Leaf(5))" >> {
+    //                s3 === 25
+    //            }
+    //            "must return '5' for Node('/', Leaf(5), Leaf(5))" >> {
+    //                s4 === 1
+    //            }
+    //            "must return '5' for Node('-', Leaf(5), Leaf(5))" >> {
+    //                s5 === 0
+    //            }
+    //            "must return '5' for Node('+', Node('-', Leaf(10), Leaf(2)), Node('*', Leaf(5), Leaf(5))" >> {
+    //                s6 === 33
+    //            }
+    //        }
+    //    }
 }
 
