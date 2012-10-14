79365f927f8643cd182f023db8cd86fe686f056c
stephan <stephan@101loops.com>
2012-10-14 23:59:07 +0200

    commit

--- src/main/scala/de/learnscala/base/Help.scala
+++ src/main/scala/de/learnscala/base/Help.scala
@@ -2,43 +2,45 @@ package de.learnscala.base
 
 trait Help {
 
-//  Wie überprüfe ich meine Übung?
-//  ------------------------------
-//
-//  Es wird empfohlen testgetrieben zu üben. Warum?
-//
-//  Zu jeder Übung existieren fertige Tests. Mit deren Hilfe sollte die Übung gelöst werden.
-//  Fehler können so schneller entdeckt werden.
-//
-//
-//
-//  Wo finde ich den jeweiligen Test ?
-//  ----------------------------------
-//
-//  Zur jeder Übung 'XYZ' gibt es eine gleichnamige Testspezifikation.
-//  Alle Tests befinden sich unter '/src/test/scala/de/learnscala/test/uebungen'.
-//
-//
-//
-//  Wie führe ich einen Test aus ?
-//  ------------------------------
-//
-//
-//  A) IDE
-//     
-//      Tests werden ausgeführt wie JUnit-Tests auch.
-//
-//     
-//  B) Konsole
-//     
-//      1.) Um einen spezifischen Test in SBT auszuführen, starten Sie zunächst SBT in der Konsole:
-//     
-//          > sbt
-//     
-//      2.) Sobald SBT geladen ist, kann mit Hilfe von 'test-only' eine einzelner Test ausgeführt werden:
-//     
-//          > test-only *S_B030
-//
-//      Das '*' ist nicht zu vergessen! Es ermöglicht den Test ohne absolute Pfadangaben.
+/*
+  Wie überprüfe ich meine Übung?
+  ------------------------------
+
+  Es wird empfohlen testgetrieben zu üben. Warum?
+
+  Zu jeder Übung existieren fertige Tests. Mit deren Hilfe sollte die Übung gelöst werden.
+  Fehler können so schneller entdeckt werden.
+
+
+
+  Wo finde ich den jeweiligen Test ?
+  ----------------------------------
+
+  Zur jeder Übung 'XYZ' gibt es eine gleichnamige Testspezifikation.
+  Alle Tests befinden sich unter '/src/test/scala/de/learnscala/test/uebungen'.
+
+
+
+  Wie führe ich einen Test aus ?
+  ------------------------------
+
+
+  A) IDE
+
+      Tests werden ausgeführt wie JUnit-Tests auch.
+
+
+  B) Konsole
+
+      1.) Um einen spezifischen Test in SBT auszuführen, starten Sie zunächst SBT in der Konsole:
+
+          > sbt
+
+      2.) Sobald SBT geladen ist, kann mit Hilfe von 'test-only' ein einzelner Test ausgeführt werden:
+
+          > test-only *S_B030
+
+      Das '*' ist nicht zu vergessen! Es ermöglicht den Test ohne absolute Pfadangaben.
+*/
 
 }
