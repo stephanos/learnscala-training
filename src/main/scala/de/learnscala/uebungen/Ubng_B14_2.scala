b2fd28652082ca533f19cef58812db2a9fb382ac
stephan <stephan@101loops.com>
2013-01-12 13:42:44 +0100

    commit

--- src/main/scala/de/learnscala/uebungen/Ubng_B14_2.scala
+++ src/main/scala/de/learnscala/uebungen/Ubng_B14_2.scala
@@ -5,134 +5,134 @@ import de.learnscala.base._
 
 class Ubng_B14_2 extends Exercise with Help /* with Disabled */ {
 
+  /**
+   * ANLEITUNG
+   *
+   * 1) Test ausführen
+   * 2) Fehler beseitigen
+   * 3) zurück zu Schritt 1
+   *
+   **/
+
+
+  // ===============================================================
+
+  task(1) {
+
     /**
-     * ANLEITUNG
+     * Methode 'nameOfSymbol'
      *
-     * 1) Test ausführen
-     * 2) Fehler beseitigen
-     * 3) zurück zu Schritt 1
+     * @param symbol mathematisches Symbol
+     * @return engl. Name des Symbols
+     */
+
+    def nameOfSymbol(symbol: Char) =
+      TODO
+  }
+
+  // ===============================================================
+
+  task(2) {
+
+    /**
+     * Methode 'isWorkingDay'
      *
-     **/
+     * @param day engl. Name des Wochentags
+     * @return ob der Wochentag ein Arbeitstag ist
+     *
+     * @todo Methode muss kürzer und lesbarer werden!
+     */
 
-
-    // ===============================================================
-
-    task(1) {
-
-        /**
-         * Methode 'nameOfSymbol'
-         *
-         * @param symbol mathematisches Symbol
-         * @return engl. Name des Symbols
-         */
-
-        def nameOfSymbol(symbol: Char) =
-            TODO
+    def isWorkingDay(day: String) = {
+      var res: Boolean = false
+      day match {
+        case "Monday" =>
+          res = true
+        case "Tuesday" =>
+          res = true
+        case "Wednesday" =>
+          res = true
+        case "Thursday" =>
+          res = true
+        case "Friday" =>
+          res = true
+        case _ =>
+      }
+      res
     }
+  }
 
-    // ===============================================================
+  // ===============================================================
 
-    task(2) {
+  task(3) {
 
-        /**
-         * Methode 'isWorkingDay'
-         *
-         * @param day engl. Name des Wochentags
-         * @return ob der Wochentag ein Arbeitstag ist
-         *
-         * @todo Methode muss kürzer und lesbarer werden!
-         */
+    /**
+     * Methode 'describeValue'
+     *
+     * @param value ein Wert
+     * @return engl. Beschreibung des Wertes
+     */
 
-        def isWorkingDay(day: String) = {
-            var res: Boolean = false
-            day match {
-                case "Monday" =>
-                    res = true
-                case "Tuesday" =>
-                    res = true
-                case "Wednesday" =>
-                    res = true
-                case "Thursday" =>
-                    res = true
-                case "Friday" =>
-                    res = true
-                case _ =>
-            }
-            res
-        }
-    }
+    def describeValue(value: Any) =
+      TODO
+  }
 
-    // ===============================================================
+  // ===============================================================
 
-    task(3) {
+  task(4) {
 
-        /**
-         * Methode 'describeValue'
-         *
-         * @param value ein Wert
-         * @return engl. Beschreibung des Wertes
-         */
+    /**
+     * Methode 'half'
+     *
+     * @param n ganze Zahl
+     * @return Hälfte der Zahl
+     * @throws RuntimeException falls der Eingabewert nicht ganzzahlig teilbar ist
+     */
 
-        def describeValue(value: Any) =
-            TODO
-    }
+    def half(n: Int) =
+      TODO
+  }
 
-    // ===============================================================
+  // ===============================================================
 
-    task(4) {
+  task(5) {
 
-        /**
-         * Methode 'half'
-         *
-         * @param n ganze Zahl
-         * @return Hälfte der Zahl
-         * @throws RuntimeException falls der Eingabewert nicht ganzzahlig teilbar ist
-         */
+    /**
+     * Methode 'getSize'
+     *
+     * @param s eine Zeichenkette
+     * @return Länge des Strings
+     */
 
-        def half(n: Int) =
-            TODO
-    }
+    def getSize(s: String) =
+      TODO
+  }
 
-    // ===============================================================
+  // ===============================================================
 
-    task(5) {
+  TODO
 
-        /**
-         * Methode 'getSize'
-         *
-         * @param s eine Zeichenkette
-         * @return Länge des Strings
-         */
+  // ===============================================================
 
-        def getSize(s: String) =
-            TODO
-    }
+  task(6) {
 
-    // ===============================================================
-
-    TODO
-
-    // ===============================================================
-
-    task(6) {
-
-        /**
-         * BONUS-AUFGABE
-         *
-         * Methode 'readCharFromFile'
-         *
-         * @param f offener java.io.FileReader
-         * @return erste Zeichen der Datei - oder '0' bei einem Fehler
-         *
-         * @note
+    /**
+     * BONUS-AUFGABE
+     *
+     * Methode 'readCharFromFile'
+     *
+     * @param f offener java.io.FileReader
+     * @return erste Zeichen der Datei - oder '0' bei einem Fehler
+     *
+     * @note
          * Printet 'cannot read' bei 'IOException'
-         * Printet 'not found' bei 'RuntimeException'
-         * Printet 'not found' bei 'FileNotFoundException'
-         *
-         * Und nicht vergessen den Reader zu schließen!
-         */
+     *     Printet 'not found' bei 'RuntimeException'
+     *     Printet 'not found' bei 'FileNotFoundException'
+     *
+     *     Und nicht vergessen den Reader zu schließen!
+     */
 
-        def readCharFromFile(f: FileReader) =
-            TODO
-    }
+    def readCharFromFile(f: FileReader) =
+      TODO
+  }
 }
