a863bd3c8027dbb07bdb853e8ad61c89a856fb33
stephan <stephan@101loops.com>
2013-01-18 18:59:45 +0100

    commit

--- src/main/scala/de/learnscala/uebungen/Ubng_F24_2.scala
+++ src/main/scala/de/learnscala/uebungen/Ubng_F24_2.scala
@@ -5,4 +5,37 @@ import de.learnscala.base._
 class Ubng_F24_2 extends Exercise with Help /* with Disabled */ {
 
   TODO
+
+  /*
+  val chars = 'a' to 'z'
+  val perms = chars flatMap { a =>
+    chars flatMap { b =>
+      if (a != b) Seq("%c%c".format(a, b))
+      else Seq()
+    }
+  }
+
+  val perms = for {
+    a <- chars
+    b <- chars
+    if a != b
+  } yield "%c%c".format(a, b)
+
+
+  val host: Option[String] = ..
+  val port: Option[Int] = ..
+
+  val addr: Option[InetSocketAddress] =
+    host flatMap { h =>
+      port map { p =>
+        new InetSocketAddress(h, p)
+      }
+    }
+
+    val addr: Option[InetSocketAddress] = for {
+      h <- host
+      p <- port
+    } yield new InetSocketAddress(h, p)
+
+  */
 }
