42e05c21e3f2ad7d530151dd3f5979f90be4297d
stephan <stephan@101loops.com>
2013-01-19 13:17:57 +0100

    commit

---
+++ src/test/scala/de/learnscala/test/beispiele/FlatSpec.scala
@@ -0,0 +1,33 @@
+package de.learnscala.test.beispiele
+
+import org.scalatest
+import collection.mutable.ListBuffer
+import scalatest.BeforeAndAfter
+
+class FlatSpec extends scalatest.FlatSpec with BeforeAndAfter {
+
+  val builder = new StringBuilder
+  val buffer = new ListBuffer[String]
+
+  before {
+    builder.append("ScalaTest is ")
+  }
+
+  after {
+    builder.clear()
+    buffer.clear()
+  }
+
+  "Testing" should "be easy" in {
+    builder.append("easy!")
+    assert(builder.toString === "ScalaTest is easy!")
+    assert(buffer.isEmpty)
+    buffer += "sweet"
+  }
+
+  it should "be fun" in {
+    builder.append("fun!")
+    assert(builder.toString === "ScalaTest is fun!")
+    assert(buffer.isEmpty)
+  }
+}
