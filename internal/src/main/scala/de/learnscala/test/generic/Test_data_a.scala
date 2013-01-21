9facfeedba8d23c5de199ff369568dfa13e878e2
stephan <stephan@101loops.com>
2013-01-21 20:59:59 +0100

    commit

--- internal/src/main/scala/de/learnscala/test/generic/Test_data_a.scala
+++ internal/src/main/scala/de/learnscala/test/generic/Test_data_a.scala
@@ -6,40 +6,27 @@ import scala.reflect.runtime.universe._
 abstract class Test_data_a[T: TypeTag] extends BaseTest[T]() {
 
   override def fs =
-    task(1)("Task #1") {
-      implicit ctx =>
-        mustReturnMethod("r1", true) ^
-          mustReturnMethod("r2", true) ^
-          mustReturnMethod("r3", true) ^
-          mustReturnMethod("r4", true) ^
-          mustReturnMethod("r5", true) ^
-          mustReturnMethod("r6", true)
-    } ^ task(2)("Seasons") {
+    task(1)("Seasons") {
       implicit ctx =>
         mustReturnMethod("t1", 0)
         mustReturnMethod("t2", 1)
         mustReturnMethod("t3", 3)
         mustReturnMethod("t4", "Herbst")
-    } ^ task(3)("Quiz #2") {
-      implicit ctx =>
-        mustReturnMethod("r1", true) ^
-          mustReturnMethod("r2", true) ^
-          mustReturnMethod("r3", true)
-    } ^ task(4)("swap", "method") {
+    } ^ task(2)("swap", "method") {
       implicit ctx =>
         mustHaveMethod {
           implicit m =>
             mustReturn((1, 5), (5, 1)) ^
               mustReturn((1, 1), (1, 1))
         }
-    } ^ task(5)("charWithCode", "method") {
+    } ^ task(3)("charWithCode", "method") {
       implicit ctx =>
         mustHaveMethod {
           implicit m =>
             mustReturn((97 -> 'a'), 'a') ^
               mustReturn((122 -> 'z'), 'z')
         }
-    } ^ task(6)("sum", "method") {
+    } ^ task(4)("sum", "method") {
       implicit ctx =>
         mustHaveMethod {
           implicit m =>
--- src/main/scala/de/learnscala/uebungen/Ubng_data_a.scala
+++ src/main/scala/de/learnscala/uebungen/Ubng_data_a.scala
@@ -4,46 +4,12 @@ import de.learnscala.base._
 
 class Ubng_data_a extends Exercise with Help /* with Disabled */ {
 
-  object Categories extends Enumeration {
-    val Easy = Value("easy")
-    val Average = Value("average")
-    val Difficult = Value("difficult")
-  }
-
-  task(1) {
-
-    /**
-     * Bewältigen Sie das Quiz:
-     * Ersetzen Sie den rechten Teil des Vergleichs mit dem entsprechenden Ergebnis!
-     */
-
-    def r1 =
-      Categories.Easy.id == TODO
-
-    def r2 =
-      Categories.Difficult.id == TODO
-
-    def r3 =
-      (Categories.Difficult.id == Categories.maxId - 1) == TODO
-
-    def r4 =
-      (Categories.Easy == Categories.Difficult) == TODO
-
-    def r5 =
-      Categories(0) == TODO
-
-    def r6 =
-      Categories.Easy.toString == TODO
-  }
-
-  // ===============================================================
-
   /**
    * Vervollständigen Sie 'Seasons', so dass die Testfälle erfolgreich laufen.
    */
   object Seasons extends Enumeration
 
-  task(2) {
+  task(1) {
     def t1 = TODO //Seasons.spring.id
     def t2 = TODO //Seasons.summer.id
     def t4 = TODO //Seasons.winter.id
@@ -52,28 +18,7 @@ class Ubng_data_a extends Exercise with Help /* with Disabled */ {
 
   // ===============================================================
 
-  task(3) {
-
-    /**
-     * Bewältigen Sie das Quiz:
-     * Ersetzen Sie den rechten Teil des Vergleichs mit dem entsprechenden Ergebnis!
-     */
-
-    def r1 =
-      ("Bayern" -> "München")._1 == TODO
-
-    def r2 =
-      ("Bayern", "München")._2 == TODO
-
-    def r3 = {
-      val t = ("Martin", 42)
-      t._1 + " ist " + t._2
-    } == TODO
-  }
-
-  // ===============================================================
-
-  task(4) {
+  task(2) {
 
     /**
      * Tauscht die Elemente des Tuples.
@@ -86,7 +31,7 @@ class Ubng_data_a extends Exercise with Help /* with Disabled */ {
 
   // ===============================================================
 
-  task(5) {
+  task(3) {
 
     /**
      * Liefert zu einem Zeichen den entsprechenden ASCII-Code
@@ -100,7 +45,7 @@ class Ubng_data_a extends Exercise with Help /* with Disabled */ {
 
   // ===============================================================
 
-  task(6) {
+  task(4) {
 
     /**
      * Summiert die Elemente eines Tuples.
