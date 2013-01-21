db3b0a5ec7b8c50ce566efbc1d5dacbb36a56dbd
stephan <stephan@101loops.com>
2013-01-21 18:31:00 +0100

    commit

--- internal/src/main/scala/de/learnscala/test/generic/Test_func3_b.scala
+++ internal/src/main/scala/de/learnscala/test/generic/Test_func3_b.scala
@@ -10,36 +10,41 @@ abstract class Test_func3_b[T: TypeTag] extends BaseTest[T]() {
       implicit ctx =>
         mustHaveMethod {
           implicit m =>
-            mustReturn(List(2, 4), List(1,2,3,4)) ^
-              mustNotBeLongerThan(2) 
+            mustReturn(List(2, 4), List(1, 2, 3, 4)) ^
+              mustNotBeLongerThan(2)
         }
     } ^
       task(2)("m2", "method") {
         implicit ctx =>
           mustHaveMethod {
             implicit m =>
-              mustNotBeLongerThan(1)
+              mustReturn((List("jaja"), List("oh")), List("oh", "jaja")) ^
+                mustReturn((List("so so"), List("aha")), List("aha", "so so")) ^
+                mustReturn((List("So so"), List("Aha")), List("Aha", "So so")) ^
+                mustReturn((List(), List()), List()) ^
+                mustNotBeLongerThan(2)
           }
       } ^
       task(3)("m3", "method") {
         implicit ctx =>
           mustHaveMethod {
             implicit m =>
-              mustNotBeLongerThan(1)
+              mustReturn(List(), List("oh", "jaja")) ^
+                mustNotBeLongerThan(2)
           }
       } ^
       task(4)("m4", "method") {
         implicit ctx =>
           mustHaveMethod {
             implicit m =>
-              mustNotBeLongerThan(1)
+              mustNotBeLongerThan(2)
           }
       } ^
       task(5)("m5", "method") {
         implicit ctx =>
           mustHaveMethod {
             implicit m =>
-              mustNotBeLongerThan(1)
+              mustNotBeLongerThan(2)
           }
       }
 }
