3f18f59b461b96160345d4834c9a8afcecd61e73
stephan <stephan@101loops.com>
2013-01-23 19:53:45 +0100

    commit

---
+++ public/src/main/scala/de/learnscala/uebungen/Ubng_func3_a.scala
@@ -0,0 +1,62 @@
+package de.learnscala.uebungen
+
+import de.learnscala.base._
+
+class Ubng_func3_a extends Exercise with Help /* with Disabled */ {
+
+  task {
+
+    /**
+     * Lösen Sie die folgenden Aufhaben mit Hilfe der 'map' Funktion!
+     *
+     * @note Vermeiden Sie Redundanz.
+     */
+
+    // ===============================================================
+
+    /**
+     * Erhöht alle Elemente der Liste um 1
+     */
+
+    def r1(l: List[Int]) =
+      FILL_IN_BODY_HERE
+
+    // ===============================================================
+
+    /**
+     * Erhöht alle Elemente der Liste um 1 und verdoppelt sie anschließend
+     */
+
+    def r2(l: List[Int]) =
+      FILL_IN_BODY_HERE
+
+    // ===============================================================
+
+    /**
+     * Verdoppelt alle Elemente der Liste und addiert dann 1 dazu
+     */
+
+    def r3(l: List[Int]) =
+      FILL_IN_BODY_HERE
+
+    // ===============================================================
+
+    /**
+     * Addiert 1 auf die Listenelemente, verdoppelt sie dann und zieht wieder 1 ab
+     */
+
+    def r4(l: List[Int]) =
+      FILL_IN_BODY_HERE
+
+    // ===============================================================
+
+    /**
+     * Addiert 1 auf das erste Element des Tuples und zieht 1 vom zweiten ab
+     */
+
+    def r5(l: List[(Int, Int)]) =
+      FILL_IN_BODY_HERE
+
+  }
+
+}
