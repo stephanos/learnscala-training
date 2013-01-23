4ad8af0b4f4cf52c3d5df0dc09735376dfd607cf
stephan <stephan@101loops.com>
2013-01-23 19:52:35 +0100

    commit

--- public/src/main/scala/de/learnscala/loesungen/Loes_F23_1.scala
+++ public/src/main/scala/de/learnscala/loesungen/Loes_F23_1.scala
@@ -1,43 +1,43 @@
-package de.learnscala.loesungen
-
-import de.learnscala.base.Solution
-
-class Loes_F23_1 extends Solution {
-
-  task {
-
-    def add1 =
-      (x: Int) => { x + 1 }
-
-    def sub1 =
-      (x: Int) => { x - 1 }
-
-    def double =
-      (x: Int) => { x * 2 }
-
-    // ===============================================================
-
-    def r1(l: List[Int]) =
-      l map { i => add1(i) }
-
-    // ===============================================================
-
-    def r2(l: List[Int]) =
-      l map { i => add1(i) } map { i => double(i) }
-
-    // ===============================================================
-
-    def r3(l: List[Int]) =
-      l map { i => double(i) } map { i => add1(i) }
-
-    // ===============================================================
-
-    def r4(l: List[Int]) =
-      l map { i => add1(i) } map { i => double(i) } map { i => sub1(i) }
-
-    // ===============================================================
-
-    def r5(l: List[(Int, Int)]) =
-      l map { t => (add1(t._1), sub1(t._2)) }
-  }
-}
+//package de.learnscala.loesungen
+//
+//import de.learnscala.base.Solution
+//
+//class Loes_F23_1 extends Solution {
+//
+//  task {
+//
+//    def add1 =
+//      (x: Int) => { x + 1 }
+//
+//    def sub1 =
+//      (x: Int) => { x - 1 }
+//
+//    def double =
+//      (x: Int) => { x * 2 }
+//
+//    // ===============================================================
+//
+//    def r1(l: List[Int]) =
+//      l map { i => add1(i) }
+//
+//    // ===============================================================
+//
+//    def r2(l: List[Int]) =
+//      l map { i => add1(i) } map { i => double(i) }
+//
+//    // ===============================================================
+//
+//    def r3(l: List[Int]) =
+//      l map { i => double(i) } map { i => add1(i) }
+//
+//    // ===============================================================
+//
+//    def r4(l: List[Int]) =
+//      l map { i => add1(i) } map { i => double(i) } map { i => sub1(i) }
+//
+//    // ===============================================================
+//
+//    def r5(l: List[(Int, Int)]) =
+//      l map { t => (add1(t._1), sub1(t._2)) }
+//  }
+//}
